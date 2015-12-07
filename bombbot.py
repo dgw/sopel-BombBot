5983f305e3ca74e3171248d3d152bc61db6aa0e6
dgw <dgw@technobabbl.es>
2015-12-07 02:26:26 -0600

    Nuke .bombnickmerge; it's its own module now
    Moved to [willie-dgwmergeutils](https://github.com/dgw/willie-dgwmergeutils),
    renamed to just .nickmerge, and (being) extended to merge my other modules' stats too.

--- bombbot.py
+++ bombbot.py
@@ -78,10 +78,6 @@ STRINGS = {
     'BOMBS_DISABLED':         "Bombs disabled in %s.",
     'ADMIN_ENABLE_BOMBS':     "Only a channel admin or greater can enable bombing in this channel.",
     'BOMBS_ENABLED':          "Bombs enabled in %s.",
-    'OWNER_MERGE':            "Only the bot owner can merge users.",
-    'MERGE_SYNTAX':           "I want to be sure there are no mistakes here. "
-                              "Please specify nicks to merge as: <duplicate> into <primary>",
-    'MERGE_DONE':             "Merged %s into %s.",
 }
 
 BOMBS = {}
@@ -409,28 +405,3 @@ def bomboprah(bot, trigger):
     bot.db.set_channel_value(trigger.sender, 'bombs_disabled', False)
     bot.say(STRINGS['BOMBS_ENABLED'] % trigger.sender)
 
-
-@commands('bombnickmerge')
-@example(".bombnickmerge newbie into old_friend")
-@require_owner(STRINGS['OWNER_MERGE'])
-def is_really(bot, trigger):
-    """
-    Merge the two nicks, keeping the stats for the second one.
-    """
-    duplicate = trigger.group(3) or None
-    primary = trigger.group(5) or None
-    if not primary or not duplicate or trigger.group(4).lower() != 'into':
-        bot.reply(STRINGS['MERGE_SYNTAX'])
-        return
-    duplicate = Identifier(duplicate)
-    primary = Identifier(primary)
-    newstats = dict()
-    for stat in ('bomb_wrongs', 'bomb_timeouts', 'bomb_defuses', 'bomb_alls', 'bombs_planted'):
-        dupval = bot.db.get_nick_value(duplicate, stat) or 0
-        prival = bot.db.get_nick_value(primary, stat) or 0
-        newstats[stat] = dupval + prival
-    for stat in newstats:
-        bot.db.set_nick_value(primary, stat, newstats[stat])
-        bot.db.set_nick_value(duplicate, stat, 0)  # because willie < 5.4.1 doesn't merge properly
-    bot.db.merge_nick_groups(primary, duplicate)
-    bot.say(STRINGS['MERGE_DONE'] % (duplicate, primary))
